6dcc5dab01edd7648cee45a13ee2996e715fdd93
Jorge Timón <jtimon@jtimon.cc>
2016-04-06 16:36:32 +0200

    Globals: Explicitly pass const CChainParams& to UpdateTip()

--- src/main.cpp
+++ src/main.cpp
@@ -2752,8 +2752,7 @@ void PruneAndFlush() {
 }
 
 /** Update chainActive and related internal data structures. */
-void static UpdateTip(CBlockIndex *pindexNew) {
-    const CChainParams& chainParams = Params();
+void static UpdateTip(CBlockIndex *pindexNew, const CChainParams& chainParams) {
     chainActive.SetTip(pindexNew);
 
     // New best block
@@ -2795,7 +2794,9 @@ void static UpdateTip(CBlockIndex *pindexNew) {
  * Disconnect chainActive's tip. You probably want to call mempool.removeForReorg and
  * mempool.removeWithoutBranchId after this, with cs_main held.
  */
-bool static DisconnectTip(CValidationState &state, const Consensus::Params& consensusParams, bool fBare = false) {
+bool static DisconnectTip(CValidationState &state, const Consensus::Params& consensusParams, bool fBare = false)
+{
+    const CChainParams& chainparams = Params(); // TODO replace consensusParams parameter
     CBlockIndex *pindexDelete = chainActive.Tip();
     assert(pindexDelete);
     // Read block from disk.
@@ -2841,7 +2842,7 @@ bool static DisconnectTip(CValidationState &state, const Consensus::Params& cons
     }
 
     // Update chainActive and related variables.
-    UpdateTip(pindexDelete->pprev);
+    UpdateTip(pindexDelete->pprev, chainparams);
     // Get the current commitment tree
     SproutMerkleTree newSproutTree;
     SaplingMerkleTree newSaplingTree;
@@ -2917,7 +2918,7 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
     mempool.removeExpired(pindexNew->nHeight);
 
     // Update chainActive & related variables.
-    UpdateTip(pindexNew);
+    UpdateTip(pindexNew, chainparams);
     // Tell wallet about transactions that went from mempool
     // to conflicted:
     BOOST_FOREACH(const CTransaction &tx, txConflicted) {
